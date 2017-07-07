18aa7c52ac13df5184e1bf9ca8299603a4121d3f
WangMengQI <qiqi.star@163.com>
2017-07-07 17:58:24 +0800

    test1

--- FocusExample/FocusExample/ViewController.m
+++ FocusExample/FocusExample/ViewController.m
@@ -22,7 +22,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
+    //
     self.automaticallyAdjustsScrollViewInsets = NO;
     
     _hanabiCollectionViewLayout = [[FocusCollectionViewLayout alloc]init];
